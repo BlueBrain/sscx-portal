ffec1655522187eedee2042c85669ab218fe4ac9
Julien Machon <julienmachon@gmail.com>
2020-02-17 16:11:10 +0100

    adding missing section title

--- src/views/experimental/config.ts
+++ src/views/experimental/config.ts
@@ -1,5 +1,6 @@
 import { accentColors } from '../../config';
 import { Color } from '../../types';
 
+export const sectionTitle = 'Experimental Data';
 export const colorName: Color = 'yellow';
 export const primaryColor = accentColors[colorName];
