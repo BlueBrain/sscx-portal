1b7770205a31957a5f04ebe009fbab15e0e66fc2
Julien Machon <julienmachon@gmail.com>
2020-02-17 16:06:47 +0100

    microcircuits template

---
+++ src/templates/Microcircuits.tsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { useHistory } from 'react-router';
+
+import Filters from '../layouts/Filters';
+import Title from '../components/Title';
+import InfoBox from '../components/InfoBox';
+import { lorem } from '../views/Styleguide';
+import Selector from '../components/Selector';
+import MicrocircuitSelector from '../components/MicrocircuitSelector';
+import useQuery from '../hooks/useQuery';
+import { Layer, Color } from '../types';
+import Pills from '../components/Pills';
+import { BrainRegion } from '../components/BrainRegionsSelector';
+import { accentColors } from '../config';
+
+export type MicrocircuitTemplateProps = {
+  color: Color;
+  sectionTitle: string;
+};
+
+const Microcircuits: React.FC<MicrocircuitTemplateProps> = ({
+  color,
+  sectionTitle,
+}) => {
+  const query = useQuery();
+  const history = useHistory();
+
+  const addParam = (key: string, value: string): void => {
+    query.set(key, value);
+    history.push(`?${query.toString()}`);
+  };
+
+  const currentRegion: BrainRegion = query.get('brain_region') as BrainRegion;
+  const currentLayer: Layer = query.get('layer') as Layer;
+
+  const setRegion = (region: BrainRegion) => addParam('brain_region', region);
+  const setLayer = (layer: Layer) => addParam('layer', layer);
+
+  return (
+    <Filters
+      primaryColor={color}
+      backgroundAlt
+      hasData={!!currentLayer && !!currentRegion}
+    >
+      <div className="center-col">
+        <Title
+          primaryColor={color}
+          title="Microcircuits"
+          subtitle={sectionTitle}
+          hint="Select a microcircuit of interest."
+        />
+        <div>
+          <InfoBox title="Longer Text" text={lorem} />
+          <br />
+          <Pills
+            title="1. Select a subregion"
+            list={['S1FL', 'S1Sh', 'S1HL', 'S1Tr']}
+            defaultValue={currentRegion}
+            onSelect={setRegion}
+            color={color}
+          />
+        </div>
+      </div>
+      <div className="center-col">
+        <Selector title="2. Choose a layer" column>
+          <MicrocircuitSelector
+            color={accentColors[color]}
+            defaultActiveLayer={currentLayer}
+            onLayerSelected={setLayer}
+            disabled={!currentRegion}
+          />
+        </Selector>
+      </div>
+    </Filters>
+  );
+};
+
+export default Microcircuits;
--- src/views/predictions/Microcircuits.tsx
+++ src/views/predictions/Microcircuits.tsx
@@ -1,69 +1,8 @@
 import React from 'react';
-import Filters from '../../layouts/Filters';
-import Title from '../../components/Title';
-import InfoBox from '../../components/InfoBox';
-import { lorem } from '../Styleguide';
-import { primaryColor, colorName } from './config';
-import Selector from '../../components/Selector';
-import MicrocircuitSelector from '../../components/MicrocircuitSelector';
-import useQuery from '../../hooks/useQuery';
-import { useHistory } from 'react-router';
-import { Layer } from '../../types';
-import Pills from '../../components/Pills';
-import { BrainRegion } from 'src/components/BrainRegionsSelector';
 
-const Microcircuits: React.FC = () => {
-  const query = useQuery();
-  const history = useHistory();
+import MicrocircuitsTemplates from '../../templates/Microcircuits';
+import { colorName, sectionTitle } from './config';
 
-  const addParam = (key: string, value: string): void => {
-    query.set(key, value);
-    history.push(`?${query.toString()}`);
-  };
-
-  const currentRegion: BrainRegion = query.get('brain_region') as BrainRegion;
-  const currentLayer: Layer = query.get('layer') as Layer;
-
-  const setRegion = (region: BrainRegion) => addParam('brain_region', region);
-  const setLayer = (layer: Layer) => addParam('layer', layer);
-
-  return (
-    <Filters
-      primaryColor={colorName}
-      backgroundAlt
-      hasData={!!currentLayer && !!currentRegion}
-    >
-      <div className="center-col">
-        <Title
-          primaryColor={colorName}
-          title="Microcircuits"
-          subtitle="Predictions"
-          hint="Select a microcircuit of interest."
-        />
-        <div>
-          <InfoBox title="Longer Text" text={lorem} />
-          <br />
-          <Pills
-            title="1. Select a subregion"
-            list={['S1FL', 'S1Sh', 'S1HL', 'S1Tr']}
-            defaultValue={currentRegion}
-            onSelect={setRegion}
-            color={colorName}
-          />
-        </div>
-      </div>
-      <div className="center-col">
-        <Selector title="2. Choose a layer" column>
-          <MicrocircuitSelector
-            color={primaryColor}
-            defaultActiveLayer={currentLayer}
-            onLayerSelected={setLayer}
-            disabled={!currentRegion}
-          />
-        </Selector>
-      </div>
-    </Filters>
-  );
-};
-
-export default Microcircuits;
+export default () => (
+  <MicrocircuitsTemplates color={colorName} sectionTitle={sectionTitle} />
+);
