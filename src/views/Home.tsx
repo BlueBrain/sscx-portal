39bfe077db8c0517ff34ee309fac950537d846ab
Anna Traussnig <anna.traussnig@gmail.com>
2020-03-13 21:17:01 +0100

    Adjust homepage for desktop

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -12,19 +12,21 @@ const classPrefix = 'Home__';
 const Home: React.FC = () => (
   <div className={`${classPrefix}basis`}>
     <section id='section-1'>
-      <Title
-        title="Somatosensory Cortex Portal"
-        subtitle="Explore the datasets!"
-        hint="Welcome to the SSCx portal created by the <b>Blue Brain Project</b>. Come and explore both model data (in silico) and experiemental data (in vitro) of the rat brain."
-        primary
-      />
-      <div className='cta'>
-        <Link to="/experimental">
-          <Button large width={140} primary>Get started</Button>
-        </Link>
-        <Link to="/styleguide">
-          <Button large width={140}>Read paper</Button>
-        </Link>
+      <div className='title'>
+        <Title
+          title="Somatosensory Cortex Portal"
+          subtitle="Explore the datasets!"
+          hint="Welcome to the SSCx portal created by the <b>Blue Brain Project</b>. Come and explore both model data (in silico) and experiemental data (in vitro) of the rat brain."
+          primary
+        />
+        <div className='cta'>
+          <Link to="/experimental">
+            <Button primary>Get started</Button>
+          </Link>
+          <Link to="/styleguide">
+            <Button>Read paper</Button>
+          </Link>
+        </div>
       </div>
       <img src={require('../assets/images/computer.svg')} alt='computer'/>
     </section>
@@ -32,36 +34,40 @@ const Home: React.FC = () => (
     <section id='section-2'>
       <h2>Explore the data. Select and analyze. <span className='nowrap'>Download<span
         className='accent-border'/></span></h2>
-      <div className='workflow'>
-        <InfoBox1 title='Explore'
-                  teaser='Discover how the Blue Brain Project organizes collected data and extrapolates principles to rebuild digbitally reconstructed newtworks towards reconciling dicrepancies in literuture.'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.</p>
-        </InfoBox1>
-        <InfoBox1 title='Select & Analyze'
-                  teaser='Enjoy selecting wich levels of detail you wish to explore and navigate back and forth between various datasets and see how they correlate.'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.</p>
-        </InfoBox1>
-        <InfoBox1 title='Download'
-                  teaser='The Blue Brain Project has made various assets available for download throughout the portal, enjoy downloading your selected items for your own use.'>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-          <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-            Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
-            laborum.</p>
-        </InfoBox1>
+      <div className='content'>
+        <div className='workflow'>
+          <InfoBox1 title='Explore'
+                    teaser='Discover how the Blue Brain Project organizes collected data and extrapolates principles to rebuild digbitally reconstructed newtworks towards reconciling dicrepancies in literuture.'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat.</p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.</p>
+          </InfoBox1>
+          <InfoBox1 title='Select & Analyze'
+                    teaser='Enjoy selecting wich levels of detail you wish to explore and navigate back and forth between various datasets and see how they correlate.'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat.</p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.</p>
+          </InfoBox1>
+          <InfoBox1 title='Download'
+                    teaser='The Blue Brain Project has made various assets available for download throughout the portal, enjoy downloading your selected items for your own use.'>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+              dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+              ea commodo consequat.</p>
+            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+              Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est
+              laborum.</p>
+          </InfoBox1>
+        </div>
+        <div>
+          <img id='screenshot' src={require('../assets/images/screenshot.png')} alt='screenshot'/>
+        </div>
       </div>
-      <img id='screenshot' src={require('../assets/images/screenshot.png')} alt='screenshot'/>
     </section>
 
     <section id='section-3'>
