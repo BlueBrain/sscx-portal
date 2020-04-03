326b8d1551cea6f0a912d68d5f8dc9d159e17ead
Anna Traussnig <anna.traussnig@gmail.com>
2020-04-03 16:43:38 +0200

    Desktop menu

--- src/layouts/Navigation/index.tsx
+++ src/layouts/Navigation/index.tsx
@@ -4,141 +4,168 @@ import Brand from '../Brand';
 import NavDesktop from '../NavDesktop';
 import NavMobile from '../NavMobile';
 import { NavLink } from 'react-router-dom';
+import { accentColors } from '../../config';
+import SvgRegions from '../../components/Icons/Regions';
+import SvgNeuron from '../../components/Icons/Neuron';
+import SvgMicrocircuit from '../../components/Icons/Microcircuit';
+import SvgSynapse from '../../components/Icons/Synapse';
 
 const classPrefix = 'nav__';
 
 export const HomeNav: React.FC = () => (
   <ul className="home-nav">
     <li>
-      <NavLink to="/" exact>
-        Home
-      </NavLink>
-    </li>
-    <li>
       <NavLink to="/#about" exact>
         About
       </NavLink>
     </li>
+    <li>
+      <NavLink to="#" exact>
+        Explore
+      </NavLink>
+    </li>
   </ul>
 );
 
-export const SecondaryNav: React.FC = () => (
-  <ul className="secondary-nav">
-    <li>
-      <NavLink to="/experimental-data">Experimental Data</NavLink>
-      <ul>
-        <li>
-          <NavLink to="/experimental-data/layer-anatomy">Layer Anatomy</NavLink>
-        </li>
-        <li>
-          <NavLink to="/experimental-data/neuron-morphology">
-            Neuron Morphology
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/experimental-data/neuron-electrophysiology">
-            Neuron Electrophysiology
-          </NavLink>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <NavLink to="/reconstruction-data">Reconstruction Data</NavLink>
-      <ul>
-        <li>
-          <NavLink to="/reconstruction-data/brain-regions">
-            Brain Regions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/reconstruction-data/layer-anatomy">
-            Layer Anatomy
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/reconstruction-data/synaptic-pathways">
-            Synaptic Pathways
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/reconstruction-data/microcircuits">
-            Microcircuits
-          </NavLink>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <NavLink to="/digital-reconstructions">Digital Reconstructions</NavLink>
-      <ul>
-        <li>
-          <NavLink to="/digital-reconstructions/brain-regions">
-            Brain Regions
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/digital-reconstructions/layer-anatomy">
-            Layer Anatomy
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/digital-reconstructions/synaptic-pathways">
-            Synaptic Pathways
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/digital-reconstructions/microcircuits">
-            Microcircuits
-          </NavLink>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <NavLink to="/validations">Validations</NavLink>
-      <ul>
-        <li>
-          <NavLink to="/validations/brain-regions">Brain Regions</NavLink>
-        </li>
-        <li>
-          <NavLink to="/validations/layer-anatomy">Layer Anatomy</NavLink>
-        </li>
-        <li>
-          <NavLink to="/validations/synaptic-pathways">
-            Synaptic Pathways
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/validations/microcircuits">Microcircuits</NavLink>
-        </li>
-      </ul>
-    </li>
-    <li>
-      <NavLink to="/predictions">Predictions</NavLink>
-      <ul>
-        <li>
-          <NavLink to="/predictions/brain-regions">Brain Regions</NavLink>
-        </li>
-        <li>
-          <NavLink to="/predictions/layer-anatomy">Layer Anatomy</NavLink>
-        </li>
-        <li>
-          <NavLink to="/predictions/synaptic-pathways">
-            Synaptic Pathways
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/predictions/microcircuits">Microcircuits</NavLink>
-        </li>
-      </ul>
-    </li>
-  </ul>
-);
+export const SecondaryNav: React.FC = () => {
+  const [active, setActive] = React.useState('exp');
+
+  return (
+    <ul className="secondary-nav">
+      <li className={active === 'exp' && 'active'}>
+        <span style={{ backgroundColor: accentColors.yellow }}/>
+        <button onClick={() => setActive('exp')}>Experimental Data</button>
+        <ul style={{ borderLeftColor: accentColors.yellow }}>
+          <li>
+            <NavLink to="/experimental-data/layer-anatomy"><SvgRegions fill={accentColors.yellow}/>Layer
+              Anatomy</NavLink>
+          </li>
+          <li>
+            <NavLink to="/experimental-data/neuron-morphology">
+              <SvgNeuron fill={accentColors.yellow}/>Neuron Morphology
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/experimental-data/neuron-electrophysiology">
+              <SvgNeuron fill={accentColors.yellow}/>Neuron Electrophysiology
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+      <li className={active === 'rec' && 'active'}>
+        <span style={{ backgroundColor: accentColors.blue }}/>
+        <button onClick={() => setActive('rec')}>Reconstruction Data</button>
+        <ul style={{ borderLeftColor: accentColors.blue }}>
+          <li>
+            <NavLink to="/reconstruction-data/brain-regions">
+              <SvgRegions fill={accentColors.blue}/>Brain Regions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/reconstruction-data/microcircuit">
+              <SvgMicrocircuit fill={accentColors.blue}/>Microcircuit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/reconstruction-data/synaptic-pathways">
+              <SvgSynapse fill={accentColors.blue}/>Synaptic Pathways
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/reconstruction-data/neurons">
+              <SvgNeuron fill={accentColors.blue}/>Neurons
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+      <li className={active === 'dig' && 'active'}>
+        <span style={{ backgroundColor: accentColors.lavender }}/>
+        <button onClick={() => setActive('dig')}>Digital Reconstructions</button>
+        <ul style={{ borderLeftColor: accentColors.lavender }}>
+          <li>
+            <NavLink to="/digital-reconstructions/brain-regions">
+              <SvgRegions fill={accentColors.lavender}/>Brain Regions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/digital-reconstructions/microcircuit">
+              <SvgMicrocircuit fill={accentColors.lavender}/>Microcircuit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/digital-reconstructions/synaptic-pathways">
+              <SvgSynapse fill={accentColors.lavender}/>Synaptic Pathways
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/digital-reconstructions/neurons">
+              <SvgNeuron fill={accentColors.lavender}/>Neurons
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+      <li className={active === 'val' && 'active'}>
+        <span style={{ backgroundColor: accentColors.green }}/>
+        <button onClick={() => setActive('val')}>Validations</button>
+        <ul style={{ borderLeftColor: accentColors.green }}>
+          <li>
+            <NavLink to="/validations/brain-regions">
+              <SvgRegions fill={accentColors.green}/>Brain Regions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/validations/microcircuit">
+              <SvgMicrocircuit fill={accentColors.green}/>Microcircuit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/validations/synaptic-pathways">
+              <SvgSynapse fill={accentColors.green}/>Synaptic Pathways
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/validations/neurons">
+              <SvgNeuron fill={accentColors.green}/>Neurons
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+      <li className={active === 'pre' && 'active'}>
+        <span style={{ backgroundColor: accentColors.grey }}/>
+        <button onClick={() => setActive('pre')}>Predictions</button>
+        <ul style={{ borderLeftColor: accentColors.grey }}>
+          <li>
+            <NavLink to="/predictions/brain-regions">
+              <SvgRegions fill={accentColors.grey}/>Brain Regions
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/predictions/microcircuit">
+              <SvgMicrocircuit fill={accentColors.grey}/>Microcircuit
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/predictions/synaptic-pathways">
+              <SvgSynapse fill={accentColors.grey}/>Synaptic Pathways
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/predictions/neurons">
+              <SvgNeuron fill={accentColors.grey}/>Neurons
+            </NavLink>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  );
+};
 
 const Navigation: React.FC = () => {
   return (
     <div role="navigation" className={`${classPrefix}basis`}>
-      <Brand />
-      <NavDesktop />
-      <NavMobile />
+      <Brand/>
+      <NavDesktop/>
+      <NavMobile/>
     </div>
   );
 };
