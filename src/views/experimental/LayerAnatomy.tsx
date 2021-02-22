aba7a4bb391f5fb83065a4bb507b803d403e8684
Pavlo Getta <pavlo.getta@epfl.ch>
2021-02-22 12:00:08 +0100

    Clean up the exp data layer anatomy view

--- src/views/experimental/LayerAnatomy.tsx
+++ src/views/experimental/LayerAnatomy.tsx
@@ -17,23 +17,21 @@ const LayerAnatomyView = () => (
     sectionTitle={sectionTitle}
   >
     {(layer) => (
-      <>
-        <DataContainer visible={!!layer}>
-          <ESData query={layerAnatomyDataQuery()}>
-            {data => (
-              <>
-                <Collapsible title="Summary">
-                  <>{data && <LayerAnatomySummary data={data}/>}</>
-                </Collapsible>
-                <Collapsible className="mt-4" title={`Layer ${layer}`}>
-                  {data && <LayerThickness layer={layer} data={data} />}
-                  {data && <NeuralDensity layer={layer} data={data} className="mt-3"/>}
-                </Collapsible>
-              </>
-            )}
-          </ESData>
-        </DataContainer>
-      </>
+      <DataContainer visible={!!layer}>
+        <ESData query={layerAnatomyDataQuery}>
+          {data => (
+            <>
+              <Collapsible title="Summary">
+                <>{data && <LayerAnatomySummary data={data}/>}</>
+              </Collapsible>
+              <Collapsible className="mt-4" title={`Layer ${layer}`}>
+                {data && <LayerThickness layer={layer} data={data} />}
+                {data && <NeuralDensity layer={layer} data={data} className="mt-3"/>}
+              </Collapsible>
+            </>
+          )}
+        </ESData>
+      </DataContainer>
     )}
   </LayerAnatomyTemplate>
 );
