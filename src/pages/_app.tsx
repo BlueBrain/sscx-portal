97ac7733f362175e20c545867d6d675cc308f20a
Pavlo Getta <pavlo.getta@epfl.ch>
2021-04-16 23:45:57 +0200

    Use TitilliumWeb font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -23,6 +23,7 @@ function MyApp({ Component, pageProps }) {
     <NexusProvider nexusClient={nexusClient}>
       <MainLayout>
         <Head>
+          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap" />
           <script src="https://www.unpkg.com/systemjs@6.1.7/dist/system.js"></script>
           <script src="https://www.unpkg.com/systemjs@6.1.7/dist/extras/named-exports.js"></script>
           <script type="systemjs-importmap" src={`${basePath}/systemjs-importmap.json`}></script>
