135dd81f6d8d93f167a4e5a1969c3f72bf0baf09
Pavlo Getta <pavlo.getta@epfl.ch>
2021-05-06 12:08:33 +0200

    Dig rec Neurons: show data section only if a memodel is selected, fix typo

--- src/templates/Neuron.tsx
+++ src/templates/Neuron.tsx
@@ -176,7 +176,7 @@ const Neurons: React.FC<NeuronsTemplateProps> = ({
               <p>
                 We labeled single neurons with biocytin to stain their axonal and dendritic morphologies
                 to enable their 3D reconstruction and their objective classification
-                into morphological types (m-types). In addition, we also characterised the electrical firing patterns
+                into morphological types (m-types). In addition, we also characterized the electrical firing patterns
                 of these neurons to different intensities of step currents injected in the soma
                 to group their response into electrical types (e-types).
                 We then mapped the e-types expressed in each m-type
@@ -240,7 +240,7 @@ const Neurons: React.FC<NeuronsTemplateProps> = ({
         </div>
       </Filters>
 
-      <DataContainer>
+      <DataContainer visible={!!currentMemodel}>
         {currentMtype && (
           <HttpData path={mtypeFactsheetPath(currentRegion, currentMtype)}>
             {data => (
