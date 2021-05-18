951c9a39011d09a3c41a9db748a16c40d135490e
Pavlo Getta <pavlo.getta@epfl.ch>
2021-05-18 17:54:00 +0200

    Fix typo in sentry server config

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -1,5 +1,5 @@
 import * as Sentry from '@sentry/nextjs';
 
 Sentry.init({
-  dsn: SENTRY_DSNprocess.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
+  dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
 });
