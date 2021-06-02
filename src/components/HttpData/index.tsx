8c61eeaa57be69115651e5c59058f09f0616f92f
Pavlo Getta <pavlo.getta@epfl.ch>
2021-06-02 12:21:15 +0200

    HttpData component: handle exceptions
    * show a message for the user
    * report issues to Sentry

--- src/components/HttpData/index.tsx
+++ src/components/HttpData/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { captureException } from '@sentry/nextjs';
 
 
 type HttpDataProps = {
   path: string;
+  label?: string;
   children: (
     data: any,
     loading: boolean,
@@ -10,36 +12,47 @@ type HttpDataProps = {
   ) => React.ReactNode;
 };
 
-const HttpData: React.FC<HttpDataProps> = ({ path, children }) => {
+const HttpData: React.FC<HttpDataProps> = ({ path, children, label = '' }) => {
   const [state, setState] = React.useState<{
     data: any;
     loading: boolean;
     error: any;
   }>({
     data: null,
-    loading: false,
+    loading: true,
     error: null,
   });
 
   React.useEffect(() => {
-    if (path) {
-      setState({ ...state, loading: true, data: null });
-      fetch(path)
-        .then(res => res.json())
-        .then(data => setState({ ...state, data, error: false }))
-        .catch(error => setState({ ...state, error, data: null }));
-    }
+    if (!path) return;
+
+    setState({ ...state, loading: true, data: null });
+    fetch(path)
+      .then(res => {
+        if (res.ok) return res.json();
+
+        const err = new Error(`Can't fetch ${path}`);
+        captureException(err);
+        return err;
+      })
+      .then(data => setState({ ...state, data, error: null, loading: false }))
+      .catch(error => setState({ ...state, error, data: null, loading: false }));
   }, [path]);
 
   if (!path) {
     return null;
   }
 
-  if (state.loading || !state.data) {
-    return <p>loading...</p>;
+  if (state.loading) {
+    return <p>Loading {label}...</p>;
   }
+
   if (state.error) {
-    return <p>An error happened loading the data... Please try again later.</p>;
+    return (
+      <p>
+        An error happened loading the {label || 'data'}. The incident has been reported to developers.
+      </p>
+    );
   }
 
   return (
