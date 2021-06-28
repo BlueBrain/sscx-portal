cdb50b695ad902100b6423a0315f8e0a2491e8ce
Pavlo Getta <pavlo.getta@epfl.ch>
2021-06-28 21:02:24 +0200

    List component: scroll list element into the view

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 
 import { Color } from '../../types';
 
@@ -15,16 +15,42 @@ type ListProps = {
   block?: boolean;
 };
 
-
 const List: React.FC<ListProps> = ({
   title,
   list,
   value,
-  onSelect = () => { },
+  onSelect = () => {},
   color,
   block = false,
 }) => {
+  const [skipScrollIntoView, setSkipScrollIntoView] = useState(false);
+  const listContainerRef = useRef<HTMLDivElement>(null);
+
   const id = title ? title.replace(/\s/g, '') : 'no_title';
+
+  const handleSelect = (selectedValue) => {
+    setSkipScrollIntoView(true);
+    onSelect(selectedValue);
+  }
+
+  useEffect(() => {
+    if (!listContainerRef || !listContainerRef.current) return;
+    if (!value) return;
+
+    if (skipScrollIntoView) {
+      setSkipScrollIntoView(false);
+      return;
+    }
+
+    const selectedIdx = list.indexOf(value);
+    if (selectedIdx === -1) return;
+
+    const selectedEl = listContainerRef.current.children[selectedIdx] as HTMLDivElement;
+
+    selectedEl.parentElement.scrollTop = selectedEl.offsetTop;
+    // TODO: Find a clever way to scroll element into the view only when props are changed from the parent component
+    // and this change isn't triggered by this component itself, fix issue reported by linter in the line below.
+  }, [value, list])
 
   return (
     <div
@@ -33,16 +59,17 @@ const List: React.FC<ListProps> = ({
       aria-labelledby={`${classPrefixList}${id}`}
     >
       {title && <p>{title} ({list.length})</p>}
-      <div className="elements">
+      <div className="elements" ref={listContainerRef}>
         {list.map(element => {
-          const selected = value === element
+          const selected = value === element;
+
           return <div
             key={element}
             role="radio"
             aria-checked={selected}
             tabIndex={0}
             className={`${classPrefixListElement}basis ${selected ? 'selected' : ''}`}
-            onClick={() => onSelect(element)}
+            onClick={() => handleSelect(element)}
             title={element}
           >
             {element}
