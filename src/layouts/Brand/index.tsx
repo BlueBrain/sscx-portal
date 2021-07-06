98fe5306ffb041afa05b96baaf81947595482b59
Marta Pałczyńska <marta.palczynska@collibra.com>
2021-07-06 08:53:33 +0200

    Update logo to be mobile friendly

--- src/layouts/Brand/index.tsx
+++ src/layouts/Brand/index.tsx
@@ -20,8 +20,9 @@ const Brand: React.FC = () => {
         </a>
       </div>
       <Link href="/">
-        <a>
-          <h1 className="text-white" >SSCx Portal</h1>
+        <a  className="project">
+          <h2 className="text-white bbp-name" >Blue Brain Project</h2>
+          <h2 className="text-white name" >SSCx Portal</h2>
         </a>
       </Link>
     </div>
