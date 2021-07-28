fc7845e4eafae4714b0759fbe9971413845f438d
Marta Pałczyńska <palczynska.marta.projekty@gmail.com>
2021-07-28 16:26:03 +0200

    Remove comma whe unit not exist

--- src/components/Factsheet/index.tsx
+++ src/components/Factsheet/index.tsx
@@ -103,7 +103,9 @@ const FactsheetMapValueEntry: React.FC<{
 
   return (
     <div className="row mt-1">
-      <div className="col-sm-4 col-xs-6 name">{fact.name}, {unitCode}</div>
+      <div className="col-sm-4 col-xs-6 name">{fact.name}
+        <Unit value={{ unit: unitCode, name: fact.name }} addComma />
+      </div>
       <div className="col-sm-8 col-xs-12">{valueColumn}</div>
     </div>
   );
--- src/components/Unit/index.tsx
+++ src/components/Unit/index.tsx
@@ -5,20 +5,22 @@ type UnitProps = {
   value?: {
     name?: string;
     unit?: string;
-  }
+  },
+  addComma?: boolean;
 }
 
 const replacement = {
   MOhm: 'MΩ',
 };
 
-const Unit: React.FC<UnitProps> = ({ value }) => {
+const Unit: React.FC<UnitProps> = ({ value, addComma }) => {
   const unit = value?.unit || '';
   if (value.name.startsWith('No. of')) {
     return null;
   }
   return (
     <>
+      {addComma && ', '}
       {(replacement[unit] || unit)}
     </>
   );
