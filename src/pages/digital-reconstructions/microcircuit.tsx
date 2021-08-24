101a46e6ea16db8b71051fb4ef9ac7067b2d48f4
Stefano Antonel <stefanoantonel@gmail.com>
2021-08-24 16:30:30 +0200

    Modify Digital Reconstructions / Microcircuit

--- src/pages/digital-reconstructions/microcircuit.tsx
+++ src/pages/digital-reconstructions/microcircuit.tsx
@@ -11,7 +11,7 @@ export default function MicrocircuitPage() {
         <title>Microcircuit / Digital reconstructions / SSCx Portal</title>
         <meta
           name="description"
-          content="A neuronal microcircuit is the smallest functional ecosystem in any brain region that encompasses a diverse morphological and electrical assortment of neurons, and their synaptic interactions."
+          content="A neuronal microcircuit is the smallest functional ecosystem in any brain region that encompasses a diverse morphological and electrical assortment of neurons, and their synaptic interactions. Blue Brain has pioneered data-driven digital reconstructions and simulations of microcircuits to investigate how local neuronal structure gives rise to global network dynamics. These methods could be extended to digitally reconstruct microcircuits in any brain region. Here we focus on the SSCx."
         />
       </Head>
 
--- src/templates/Microcircuit.tsx
+++ src/templates/Microcircuit.tsx
@@ -84,7 +84,10 @@ const Microcircuit: React.FC<MicrocircuitTemplateProps> = ({
                 <p>
                   A neuronal microcircuit is the smallest functional ecosystem in any brain region that
                   encompasses a diverse morphological and electrical assortment of neurons,
-                  and their synaptic interactions.
+                  and their synaptic interactions. Blue Brain has pioneered data-driven digital
+                  reconstructions and simulations of microcircuits to investigate how local neuronal
+                  structure gives rise to global network dynamics. These methods could be extended to
+                  digitally reconstruct microcircuits in any brain region. Here we focus on the SSCx.
                 </p>
               </InfoBox>
             </div>
