ddaf9b15c410130e2186c98457ddfe0d4bb03f0b
Stefano Antonel <stefanoantonel@gmail.com>
2021-08-25 10:08:45 +0200

    Reorder Digital Reconstructions / Brain regions

--- src/views/digitalReconstructions/BrainRegions.tsx
+++ src/views/digitalReconstructions/BrainRegions.tsx
@@ -19,10 +19,10 @@ const subregionTableColumns = [
 ];
 
 function getSubregionDescription(subregion: string, data): string {
-  const neurons = data.find(d => d.name === 'No. of neuronss')?.value;
+  const neurons = data.find(d => d.name === 'No. of neurons')?.value || '';
   const extrSynapses = data.find(d => d.name === 'No. of extrinsic synapses')?.value;
   const intrSynapses = data.find(d => d.name === 'No. of intrinsic synapses')?.value;
-  const synapses = (extrSynapses && extrSynapses) ? extrSynapses + intrSynapses : '?';
+  const synapses = (extrSynapses && extrSynapses) ? extrSynapses + intrSynapses : '';
   
   return `The ${subregion} sub-region consists of ${neurons?.toLocaleString()} neurons
     mediated by ${synapses?.toLocaleString()} synapses.`;
@@ -37,13 +37,36 @@ const BrainRegionsView = () => (
       <DataContainer
         visible={!!subregion}
         navItems={[
-          { id: 'regionSection', label: 'Region' },
           { id: 'subregionSection', label: 'Subregion' },
+          { id: 'regionSection', label: 'Region' },
           { id: 'simulationSection', label: 'Simulations' },
         ]}
       >
         <Collapsible
+          id="subregionSection"
+          title={`${subregion} (Sub-region) Factsheet`}
+        >
+          <HttpData path={subregionCircuitFactsheetPath(subregion)}>
+            {data => (
+              <>
+                <p>{getSubregionDescription(subregion, data[0].values)}</p>
+                <Factsheet id="subregionCircuitFactsheet" facts={data[0].values} />
+                <div className="text-right mt-2">
+                  <HttpDownloadButton
+                    href={subregionCircuitFactsheetPath(subregion)}
+                    download={`subregion-circuit-factsheet-${subregion}.json`}
+                  >
+                    factsheet
+                  </HttpDownloadButton>
+                </div>
+              </>
+            )}
+          </HttpData>
+        </Collapsible>
+
+        <Collapsible
           id="regionSection"
+          className="mt-4"
           title="S1 (Region) Factsheet"
         >
           <p>The S1 consists of eight sub-regions:</p>
@@ -71,29 +94,6 @@ const BrainRegionsView = () => (
                   <HttpDownloadButton
                     href={regionCircuitFactsheetPath()}
                     download={`region-circuit-factsheet.json`}
-                  >
-                    factsheet
-                  </HttpDownloadButton>
-                </div>
-              </>
-            )}
-          </HttpData>
-        </Collapsible>
-
-        <Collapsible
-          id="subregionSection"
-          className="mt-4"
-          title={`${subregion} (Sub-region) Factsheet`}
-        >
-          <HttpData path={subregionCircuitFactsheetPath(subregion)}>
-            {data => (
-              <>
-                <p>{getSubregionDescription(subregion, data[0].values)}</p>
-                <Factsheet id="subregionCircuitFactsheet" facts={data[0].values} />
-                <div className="text-right mt-2">
-                  <HttpDownloadButton
-                    href={subregionCircuitFactsheetPath(subregion)}
-                    download={`subregion-circuit-factsheet-${subregion}.json`}
                   >
                     factsheet
                   </HttpDownloadButton>
