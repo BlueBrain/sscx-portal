61bd6bece176f23103493e1e96e9f67ff93f7979
Pavlo Getta <pavlo.getta@epfl.ch>
2021-09-29 20:27:52 +0200

    Collapse mtype colormap

---
+++ src/components/MtypeLegend/colormap.ts
@@ -0,0 +1,45 @@
+
+const colorMap = [
+  {
+    color: [11, 124, 68],
+    mtypes: ['DBC'],
+  },
+  {
+    color: [124, 39, 125],
+    mtypes: ['BTC'],
+  },
+  {
+    color: [127, 20, 22],
+    mtypes: ['NGC'],
+  },
+  {
+    color: [134, 204, 232],
+    mtypes: ['BP'],
+  },
+  {
+    color: [138, 70, 31],
+    mtypes: ['DAC', 'HAC', 'LAC', 'SAC'],
+  },
+  {
+    color: [184, 82, 158],
+    mtypes: ['SS'],
+  },
+  {
+    color: [232, 32, 36],
+    mtypes: ['BPC', 'HPC', 'IPC', 'TPC', 'UPC'],
+  },
+  {
+    color: [245, 234, 20],
+    mtypes: ['MC'],
+  },
+  {
+    color: [249, 163, 26],
+    mtypes: ['ChC'],
+  },
+  {
+    color: [57, 83, 163],
+    mtypes: ['LBC', 'NBC', 'SBC'],
+  }
+];
+
+export default colorMap;
--- src/components/MtypeLegend/index.tsx
+++ src/components/MtypeLegend/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import { uniq } from 'lodash';
 
-import mtypeColormap from '../../mtype-colormap.json';
+import colorMap from './colormap';
 
 import style from './styles.module.scss';
 
@@ -10,87 +9,31 @@ type MtypeLegendProps = {
   className?: string;
 };
 
-const mtypeLayerRe = /^(L\d+)*/;
+const getCellBgColorStr = (rgb: number[]) => `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
 
 const MtypeLegend: React.FC<MtypeLegendProps> = ({ className }) => {
-  const mtypeCollection = Object.entries(mtypeColormap)
-    .map(([mtype, rgb]) => {
-      const mtypeLayer = mtype.match(mtypeLayerRe)[1];
-      const layer = mtypeLayer.match(/2|3/) ? 'L23' : mtypeLayer;
-
-      return {
-        mtype,
-        layer,
-        rgb,
-        rgbKey: rgb.join('_'),
-      }
-    })
-    .sort((color1, color2) => color1.mtype < color2.mtype ? -1 : 1);
-
-  const rgbKeys = uniq(mtypeCollection.map(o => o.rgbKey)).sort();
-  const layers = uniq(mtypeCollection.map(o => o.layer)).sort();
-
-  const mtypeCollectionGrouped = {};
-  mtypeCollection.forEach(mtype => {
-    const { layer, rgbKey } = mtype;
-
-    if (!mtypeCollectionGrouped[layer]) {
-      mtypeCollectionGrouped[layer] = {};
-    }
-
-    if (!mtypeCollectionGrouped[layer][rgbKey]) {
-      mtypeCollectionGrouped[layer][rgbKey] = [mtype];
-    } else {
-      mtypeCollectionGrouped[layer][rgbKey].push(mtype);
-    }
-  });
-
-  const getCellBgColorStr = (rgbKey) => {
-    const rgb = rgbKey.split('_').map(numStr => parseInt(numStr, 10));
-    return `rgb(${rgb[0]}, ${rgb[1]}, ${rgb[2]})`;
-  };
-
   return (
     <div id="mtypeLegend" className={className}>
       <h2>M-type legend</h2>
-      <div className={style.containerDesktop}>
-        {layers.map(layer => (
-          <div key={layer} className={style.row}>
-            <div className="row">
-              {rgbKeys.map(rgbKey => (
-                <div key={rgbKey} className="col-xs-2 col-sm mt-1 mb-1">
-                  {!!mtypeCollectionGrouped[layer][rgbKey] && (
-                    <>
-                      <div className={style.colorBlock} style={{ backgroundColor: getCellBgColorStr(rgbKey) }} />
-                      {mtypeCollectionGrouped[layer][rgbKey].map(mtype => (
-                        <div className={style.label} key={mtype.mtype}>{mtype.mtype}</div>
-                      ))}
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
-          </div>
-        ))}
-      </div>
 
-      <div className={style.containerMobile}>
-        {rgbKeys.map(rgbKey => (
-          <div key={rgbKey} className={style.row}>
-            <div className="row">
-              {layers.map(layer => (
-                <div key={layer} className="col-xs-2 col-sm mt-1 mb-1">
-                  {!!mtypeCollectionGrouped[layer][rgbKey] && (
-                    <>
-                      <div className={style.colorBlock} style={{ backgroundColor: getCellBgColorStr(rgbKey) }} />
-                      {mtypeCollectionGrouped[layer][rgbKey].map(mtype => (
-                        <div className={style.label} key={mtype.mtype}>{mtype.mtype}</div>
-                      ))}
-                    </>
-                  )}
-                </div>
-              ))}
-            </div>
+      <div className={`row ${style.container}`}>
+        {colorMap.map(entry => (
+          <div
+            className="text-center col-xs-2 col-sm mt-1"
+            key={entry.color.join('')}
+          >
+            <div
+              className={style.colorBlock}
+              style={{ backgroundColor: getCellBgColorStr(entry.color) }}
+            />
+            {entry.mtypes.map(mtype => (
+              <div
+                className={style.label}
+                key={mtype}
+              >
+                {mtype}
+              </div>
+            ))}
           </div>
         ))}
       </div>
