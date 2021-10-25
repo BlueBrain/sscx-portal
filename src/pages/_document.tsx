da06b5da09dea4f947c4cd4d1f9573c0782a5f35
Pavlo Getta <pavlo.getta@epfl.ch>
2021-10-25 13:04:59 +0200

    Add preconnect for critical path assets

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,6 +8,10 @@ class AppDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+          <link rel="preconnect" href="https://www.unpkg.com/" />
+
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap"
@@ -17,6 +21,7 @@ class AppDocument extends Document {
           <script src="https://www.unpkg.com/systemjs@6.1.7/dist/extras/named-exports.js"></script>
           <script type="systemjs-importmap" src={`${basePath}/systemjs-importmap.json`}></script>
         </Head>
+
         <body>
           <Main />
           <NextScript />
