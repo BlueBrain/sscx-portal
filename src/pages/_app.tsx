579ed7630b474876261d83ff55e20698b30fbfc7
Pavlo Getta <pavlo.getta@epfl.ch>
2021-10-26 21:07:30 +0200

    Bundle systemjs with webpack

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,7 @@ if (typeof window === 'undefined') {
   require('abort-controller/polyfill');
 } else {
   smoothscroll.polyfill();
+  require('systemjs');
 }
 
 
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,15 +10,12 @@ class AppDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-          <link rel="preconnect" href="https://www.unpkg.com/" />
 
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap"
           />
 
-          <script src="https://www.unpkg.com/systemjs@6.1.7/dist/system.js"></script>
-          <script src="https://www.unpkg.com/systemjs@6.1.7/dist/extras/named-exports.js"></script>
           <script type="systemjs-importmap" src={`${basePath}/systemjs-importmap.json`}></script>
         </Head>
 
