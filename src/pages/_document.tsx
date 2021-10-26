23f076906e40cd98f2a8a61d7caa65c577bf4680
Pavlo Getta <pavlo.getta@epfl.ch>
2021-10-26 21:45:54 +0200

    Embed google fonts stylesheet into the main css
    This is a temporary solution while the next.js issue with font optimisation
    while behind a proxy isn't fixed. See https://github.com/vercel/next.js/issues/25556

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -8,14 +8,6 @@ class AppDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-
-          <link
-            rel="stylesheet"
-            href="https://fonts.googleapis.com/css2?family=Titillium+Web:wght@300;400;700&display=swap"
-          />
-
           <script type="systemjs-importmap" src={`${basePath}/systemjs-importmap.json`}></script>
         </Head>
 
