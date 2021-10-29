a097c00b85a7cc0fc65c3468d683f572a64adb95
Pavlo Getta <pavlo.getta@epfl.ch>
2021-10-29 12:25:02 +0200

    Enable performance monitoring via Sentry

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -3,4 +3,5 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN,
   environment: process.env.ENVIRONMENT || process.env.NEXT_PUBLIC_ENVIRONMENT,
+  tracesSampleRate: 0.2,
 });
