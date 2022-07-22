6385785ff113e0a3aab49728733f89563ce3b3b2
Pavlo Getta <pavlo.getta@epfl.ch>
2022-07-22 09:34:10 +0200

    Video encoding script: add 4k UHD, colored output and script help

--- scripts/gen-video-versions.sh
+++ scripts/gen-video-versions.sh
@@ -2,10 +2,20 @@
 
 set -e
 
+if [[ -z $1 ]]; then
+  echo "Script sage:"
+  echo "$ ./gen-video-versions.sh <source_video_path> <output_path> <encoding_format>(optional) <size>(optional)"
+  echo "Where optional arguments can have the following values:"
+  echo "  encoding_format: all, h264, hevc, av1"
+  echo "  size:            all, default (everything except 2160p), 2160p, 1080p, 720p, 480p, 360p"
+  exit 1
+fi
+
+
 SOURCE_VIDEO_PATH=$(realpath $1)
 OUTPUT_PATH=$(realpath ${2:-./})
 ENCODING_FORMAT=${3:-all}
-SIZE=${4:-all}
+SIZE=${4:-default}
 
 SOURCE_VIDEO_FILENAME=$(basename $SOURCE_VIDEO_PATH)
 
@@ -15,8 +25,8 @@ if [ "$ENCODING_FORMAT" != "all" ] && [ "$ENCODING_FORMAT" != "h264" ] && [ "$EN
   exit 1
 fi
 
-if [ "$SIZE" != "all" ] && [ "$SIZE" != "1080p" ] && [ "$SIZE" != "720p" ] && [ "$SIZE" != "480p" ] && [ "$SIZE" != "360p" ]; then
-  echo "Unknown size provided: $SIZE. Supported values: all, 1080p, 720p, 480p, 360p"
+if [ "$SIZE" != "default" ] && [ "$SIZE" != "all" ] && [ "$SIZE" != "2160p" ] && [ "$SIZE" != "1080p" ] && [ "$SIZE" != "720p" ] && [ "$SIZE" != "480p" ] && [ "$SIZE" != "360p" ]; then
+  echo "Unknown size provided: $SIZE. Supported values: all, default (everything except 2160p), 2160p, 1080p, 720p, 480p, 360p"
   exit 1
 fi
 
@@ -39,15 +49,32 @@ done
 FILENAME_BASE="${SOURCE_VIDEO_FILENAME%.*}"
 
 if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "h264" ]; then
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
-    echo "Encoding 1080p H264"
+  if [ "$SIZE" = "all" ] || [ "$SIZE" = "2160p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 2160p H264$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an \
       -c:v libx264 \
-      -crf 24 \
+      -crf 22 \
+      -preset veryslow \
+      -profile:v high \
+      -pix_fmt yuv420p \
+      -movflags +faststart \
+      -vf scale=3840:-2 \
+      $OUTPUT_PATH/$FILENAME_BASE.2160p.h264.mp4
+  fi
+
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 1080p H264$(tput sgr 0)"
+
+    ffmpeg \
+      -i $SOURCE_VIDEO_PATH \
+      -map_metadata -1 \
+      -an \
+      -c:v libx264 \
+      -crf 22 \
       -preset veryslow \
       -profile:v high \
       -pix_fmt yuv420p \
@@ -56,15 +83,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "h264" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.1080p.h264.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
-    echo "Encoding 720p H264"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 720p H264$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an \
       -c:v libx264 \
-      -crf 24 \
+      -crf 22 \
       -preset veryslow \
       -profile:v high \
       -pix_fmt yuv420p \
@@ -73,15 +100,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "h264" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.720p.h264.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
-    echo "Encoding 480p H264"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 480p H264$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an \
       -c:v libx264 \
-      -crf 24 \
+      -crf 22 \
       -preset veryslow \
       -profile:v high \
       -pix_fmt yuv420p \
@@ -90,15 +117,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "h264" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.480p.h264.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
-    echo "Encoding 360p H264"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 360p H264$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an \
       -c:v libx264 \
-      -crf 24 \
+      -crf 22 \
       -preset veryslow \
       -profile:v high \
       -pix_fmt yuv420p \
@@ -109,15 +136,32 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "h264" ]; then
 fi
 
 if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "hevc" ]; then
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
-    echo "Encoding 1080p HEVC"
+  if [ "$SIZE" = "all" ] || [ "$SIZE" = "2160p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 2160p HEVC$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libx265 \
-      -crf 29 \
+      -crf 27 \
+      -preset veryslow \
+      -pix_fmt yuv420p \
+      -movflags +faststart \
+      -tag:v hvc1 \
+      -vf scale=3840:-2 \
+      $OUTPUT_PATH/$FILENAME_BASE.2160p.hevc.mp4
+  fi
+
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 1080p HEVC$(tput sgr 0)"
+
+    ffmpeg \
+      -i $SOURCE_VIDEO_PATH \
+      -map_metadata -1 \
+      -an -c:v \
+      libx265 \
+      -crf 27 \
       -preset veryslow \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -126,15 +170,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "hevc" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.1080p.hevc.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
-    echo "Encoding 720p HEVC"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 720p HEVC$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libx265 \
-      -crf 29 \
+      -crf 27 \
       -preset veryslow \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -143,15 +187,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "hevc" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.720p.hevc.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
-    echo "Encoding 480p HEVC"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 480p HEVC$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libx265 \
-      -crf 29 \
+      -crf 27 \
       -preset veryslow \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -160,15 +204,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "hevc" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.480p.hevc.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
-    echo "Encoding 360p HEVC"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 360p HEVC$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libx265 \
-      -crf 29 \
+      -crf 27 \
       -preset veryslow \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -179,15 +223,31 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "hevc" ]; then
 fi
 
 if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "av1" ]; then
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
-    echo "Encoding 1080p AV1"
+  if [ "$SIZE" = "all" ] || [ "$SIZE" = "2160p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 2160p AV1$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libsvtav1 \
-      -crf 51 \
+      -qp 48 \
+      -preset 0 \
+      -pix_fmt yuv420p \
+      -movflags +faststart \
+      -vf scale=3840:-2 \
+      $OUTPUT_PATH/$FILENAME_BASE.2160p.av1.mp4
+  fi
+
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "1080p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 1080p AV1$(tput sgr 0)"
+
+    ffmpeg \
+      -i $SOURCE_VIDEO_PATH \
+      -map_metadata -1 \
+      -an -c:v \
+      libsvtav1 \
+      -qp 48 \
       -preset 0 \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -195,15 +255,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "av1" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.1080p.av1.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
-    echo "Encoding 720p AV1"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "720p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 720p AV1$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libsvtav1 \
-      -crf 51 \
+      -qp 48 \
       -preset 0 \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -211,15 +271,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "av1" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.720p.av1.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
-    echo "Encoding 480p AV1"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "480p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 480p AV1$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libsvtav1 \
-      -crf 51 \
+      -qp 48 \
       -preset 0 \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -227,15 +287,15 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "av1" ]; then
       $OUTPUT_PATH/$FILENAME_BASE.480p.av1.mp4
   fi
 
-  if [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
-    echo "Encoding 360p AV1"
+  if [ "$SIZE" = "default" ] ||  [ "$SIZE" = "all" ] || [ "$SIZE" = "360p" ]; then
+    echo "$(tput setaf 32)$(tput setab 44)Encoding 360p AV1$(tput sgr 0)"
 
     ffmpeg \
       -i $SOURCE_VIDEO_PATH \
       -map_metadata -1 \
       -an -c:v \
       libsvtav1 \
-      -crf 51 \
+      -qp 48 \
       -preset 0 \
       -pix_fmt yuv420p \
       -movflags +faststart \
@@ -245,4 +305,4 @@ if [ "$ENCODING_FORMAT" = "all" ] || [ "$ENCODING_FORMAT" = "av1" ]; then
 fi
 
 echo
-echo Done
+echo "$(tput setaf 32)$(tput setab 42)Done$(tput sgr 0)"
