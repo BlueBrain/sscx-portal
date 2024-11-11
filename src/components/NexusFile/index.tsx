36bc5b783cb3d2683409e8ae37274665a4699f3e
Pavlo Getta <pavlo.getta@epfl.ch>
2024-11-11 11:02:28 +0100

    Fix nexus config reference

--- src/components/NexusFile/index.tsx
+++ src/components/NexusFile/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react';
 import { useNexusContext } from '@bbp/react-nexus';
 
-import { sscx } from '../../config';
+import { nexus as nexusConfig } from '../../config';
 
 
 type NexusFileProps = {
@@ -32,8 +32,8 @@ const NexusFile: React.FC<NexusFileProps> = ({
     if (hasData) {
       setState({ ...state, loading: true });
       nexus.File.get(
-        sscx.org,
-        sscx.project,
+        nexusConfig.org,
+        nexusConfig.project,
         contentUrl.split('/').reverse()[0],
         { as: 'blob' }
       )
